f40cc3dabeef3a0b3175a333f7e94b3efde9616e
kinnejs <kinnejs@gmail.com>
2020-12-13 17:31:01 -0500

    sixth upload

---
+++ 04_KomodoOutings_Repo/Outings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_KomodoOutings_Repo
+{
+    public enum OutingType
+    {
+        Golf = 1,
+        Bowling,
+        AmusementPark,
+        Concert
+    }
+    //POCO
+    public class Outings
+    {
+        public string OutingName { get; set; }
+        public OutingType TypeOfOuting { get; set; }
+        public int NumberOfPeople { get; set; }
+        public DateTime Date { get; set; }
+        public double CostPerPerson { get; set; }
+        public double TotalCost
+        {
+            get
+            {
+                return NumberOfPeople * CostPerPerson;
+            }
+
+        }
+
+        public Outings() { }
+        public Outings(string outingName, OutingType outingType, int numberOfPeople, DateTime date, double costPerPerson)
+        {
+            OutingName = outingName;
+            TypeOfOuting = outingType;
+            NumberOfPeople = numberOfPeople;
+            Date = date;
+            CostPerPerson = costPerPerson;
+        }
+        
+    }
+}
---
+++ 04_KomodoOutings_Repo/OutingsRepo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_KomodoOutings_Repo
+{
+    public class OutingsRepo
+    {
+        private List<Outings> _listOfOutings = new List<Outings>();
+
+        //Create Outing
+        public void AddOuting(Outings outing)
+        {
+            _listOfOutings.Add(outing);
+        }
+
+        //Read
+        public List<Outings> ViewOutings()
+        {
+            return _listOfOutings;
+        }
+
+        //Delete
+        public bool RemoveOuting(string name)
+        {
+            Outings outing = GetOutingByName(name);
+
+            if (outing == null)
+            {
+                return false;
+            }
+
+            int initialCount = _listOfOutings.Count;
+            _listOfOutings.Remove(outing);
+
+            if(initialCount > _listOfOutings.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
+        
+        //Helper Method
+        public Outings GetOutingByName(string name)
+        {
+            foreach(Outings outing in _listOfOutings)
+            {
+                if(outing.OutingName == name)
+                {
+                    return outing;
+                }
+            }
+            return null;
+        }
+
+    }
+}
